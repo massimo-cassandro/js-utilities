2adb82ff0032996fd1390f81f6c63dfd6d0c6ebe
Massimo Cassandro <massimo.cassandro@gmail.com>
2023-11-15 12:55:42 +0100

    1.9.4 - upd

--- react-hooks/src/useIsDesktopHook.jsx
+++ react-hooks/src/useIsDesktopHook.jsx
@@ -1,17 +1,29 @@
 import React from 'react';
 
-export function useIsDesktop(desktopBreakpoint) {
+/**
+ *
+ * @param {number} desktopBreakpoint - larghezza del viewport in px a partire della quale viene considerato desktop
+ * @param {boolean} force - Impostare a true per forzare l'esito sempre uguale a true
+ * @returns
+ */
+
+export function useIsDesktop(desktopBreakpoint, force = false) {
   const [isDesktop, updIsDesktop] = React.useState(false);
 
   React.useEffect(() => {
 
-    const mql = window.matchMedia(`(min-width: ${desktopBreakpoint}px)`),
-      mqlChangeHandler = () => {
-        updIsDesktop(mql.matches);
-      };
+    if(force) {
+      updIsDesktop(true);
+    } else {
+      const mql = window.matchMedia(`(min-width: ${desktopBreakpoint}px)`),
+        mqlChangeHandler = () => {
+          updIsDesktop(mql.matches);
+        };
 
-    mql.addEventListener('change', mqlChangeHandler);
-    mqlChangeHandler();
+      mql.addEventListener('change', mqlChangeHandler);
+      mqlChangeHandler();
+    }
+
   }, []);
 
   return isDesktop;
