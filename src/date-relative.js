656de43775af02ef9597992c461a85d81da05d55
Massimo Cassandro <massimo.cassandro@gmail.com>
2024-03-17 21:40:49 +0100

    fix

--- src/date-relative.js
+++ src/date-relative.js
@@ -1,14 +1,40 @@
+// mostra una data in forma relativa a quella corrente se entro i limiti impostati
+// le date relative sono aggiornate automaticamente ogni minuto
+
+
 export default function (date, options = {}) {
 
   // date: data da elaborare (oggetto date o stringa iso)
-  // options oggetto per la persopnalizzazione del risultato
+  // opts oggetto per la personalizzazione del risultato
 
-  // TODO: tra xx minuti, adesso,... con definizione del range
+  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/RelativeTimeFormat
+
+  // TODO opzione aggiunta ora di inizio nelle date relative
 
   const default_options = {
 
-    // La prima parola della stringa ha l'iniziale maiuscola?
-    firstLetterUppercaseCase: true,
+    // se true mostra gli orari in prossimità della data come `tra xx minuti` (prima)
+    // e `adesso` (se entro `minutesAfter`)
+    useRelativeTime: true,
+
+    // adesso
+    nowString: 'adesso',
+
+    // minuti prima della data entro i quali mostrare `tra xx minuti`
+    relativeTimeMinutesBefore: 30,
+
+    //minuti dopo la data entro i quali mostrare `adesso`
+    relativeTimeMinutesAfter: 30,
+
+    // mostra l'ora effettiva nelle date relative
+    relativeTimeShowTime: true,
+
+    // Markup da mostrare nelle date relative se `relativeTimeShowTime === true`
+    // `@@time@@` viene sotituito con l'ota nel locale impostato
+    relativeTimeShowTimeMarkup: ' <span class="time-info">(alle @@time@@)</span>',
+
+    // La prima parola della stringa restituita ha l'iniziale maiuscola?
+    firstLetterUpperCase: true,
 
     // formato data non relativa (oltre 'dopodomani')
     dateFormat: {
@@ -26,32 +52,77 @@ export default function (date, options = {}) {
       hour12: false,
       hour:'2-digit',
       minute:'2-digit'
-    }
+    },
+
+    relativeStrings: [ // NB: in ordine temporale
+      'ieri alle', 'oggi alle', 'domani alle', 'dopodomani alle'
+    ],
+
+    locale: 'it-IT'
 
   };
 
-  options = {...options, ...default_options};
+  let isRelative = false;
 
-  const d = typeof date === 'string'? new Date(date) : date,
-    now = new Date(),
-    dayDiff = d.getDate() - now.getDate();
+  const parseDate = (date, options = {}) => {
 
-  let result;
+    const opts = {...default_options, ...options};
 
-  if(dayDiff >= -1 && dayDiff <= 2) {
+    const d = typeof date === 'string'? new Date(date) : date
+      ,now = new Date()
+      ,minutesDiff = (d.getTime() - now.getTime())/60000 // differenza tra le due date in minuti
+      ,minutesPerDay = 1440
+      ,daysDiff = Math.floor(minutesDiff/minutesPerDay)// differenza tra le due date in giorni
+      ,rtf = new Intl.RelativeTimeFormat('it', { style: 'long' })
+    ;
 
-    const relativeStrings = [ // NB: in sequenza secondo il valore di dayDiff
-      'ieri', 'oggi', 'domani', 'dopodomani'
-    ];
-    result = relativeStrings[dayDiff+1] + ' alle ' + d.toLocaleString('it-IT', options.timeFormat);
+    let result;
 
-  } else {
-    result = d.toLocaleString('it-IT', options.dateFormat);
-  }
+    if (opts.useRelativeTime && Math.floor(Math.abs(minutesDiff)) === 0) {
+
+      result = opts.nowString;
+      isRelative = true;
+
+    } else if(opts.useRelativeTime && (
+      (Math.abs(minutesDiff) <= opts.relativeTimeMinutesAfter && minutesDiff < 0) ||
+      (minutesDiff > 0 && minutesDiff <= opts.relativeTimeMinutesBefore)
+    )) {
+
+      const parts = rtf.formatToParts( minutesDiff, 'minute'); // necessario per evitare decimali nel valore
+
+      result = parts.map(i => i.type !== 'literal'? Math.round(+i.value) : i.value ).join('')+
+        (opts.relativeTimeShowTime? opts.relativeTimeShowTimeMarkup?.replace('@@time@@', d.toLocaleString(opts.locale, opts.timeFormat)) : '');
+
+      isRelative = true;
+
+    // tra -1 giorno e +2 giorni
+    } else if(daysDiff >= -1 && daysDiff <= 2) {
+
+      result = opts.relativeStrings[daysDiff+1] + ' ' + d.toLocaleString(opts.locale, opts.timeFormat);
+
+    } else {
+      result = d.toLocaleString(opts.locale, opts.dateFormat);
+    }
+
+    if(opts.firstLetterUpperCase) {
+      result = result.at(0).toUpperCase() + result.substring(1).toLowerCase();
+    }
+
+    return `<time datetime="${d.toISOString()}"${isRelative? ` data-relative-date-opts="${encodeURIComponent(JSON.stringify(options))}"` : ''}>${result}</time>`;
+
+  }; // end parseDate
+
+  const result = parseDate(date, options);
 
 
-  if(options.firstLetterUppercaseCase) {
-    result = result.at(0).toUpperCase() + result.substring(1).toLowerCase();
+  if(isRelative && !Window.relativeDateUpd) {
+    Window.relativeDateUpd = true;
+    setInterval(() => {
+
+      document.querySelectorAll('time[data-relative-date-opts]').forEach(item => {
+        item.outerHTML = parseDate(item.getAttribute('datetime'), JSON.parse(decodeURIComponent(item.dataset.relativeDateOpts)));
+      });
+    }, 60000);
   }
 
   return result;
