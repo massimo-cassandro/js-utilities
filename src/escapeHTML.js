2259dfa1cabfa9c0bcdb41048e996142b6412f8f
Massimo Cassandro <massimo.cassandro@gmail.com>
2024-05-29 16:01:38 +0200

    1.14.1 - fix

--- src/escapeHTML.js
+++ src/escapeHTML.js
@@ -6,14 +6,13 @@ const characters_to_escape = {
   '>': '&gt;'
 };
 export function escapeHTML(str) {
-  return str?.replace(/[<>&"']/g, m => characters_to_escape[m])?? '';
+  const regexp = new RegExp(`[${Object.keys(characters_to_escape).join('')}]`, 'g');
+  return str?.replace(regexp, m => characters_to_escape[m])?? '';
 }
 
 export function unescapeHTML(str) {
   const regexp = new RegExp(Object.values(characters_to_escape).join('|'), 'g'),
     entities_to_unescape = Object.fromEntries(Object.entries(characters_to_escape).map(a => a.reverse()));
 
-  return str?.replace(regexp, function(m){
-    return entities_to_unescape[m];
-  })?? '';
+  return str?.replace(regexp, m => entities_to_unescape[m] )?? '';
 }
