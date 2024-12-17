867cea24667394f106331892e7ab0de7926bff76
Massimo Cassandro <massimo.cassandro@gmail.com>
2024-12-17 10:11:56 +0100

    upd

--- src/dates-from-to.js
+++ src/dates-from-to.js
@@ -1,41 +1,44 @@
-export function datesFromTo(dateString1, dateString2) {
-  let locale = {
-    year: 'numeric',
-    month: 'short',
-    day: 'numeric'
+export function datesFromTo(dateIsoString1, dateIsoString2, options={}) {
+
+  const default_options = {
+    locale: 'it-IT',
+    locale_options: {
+      year: 'numeric',
+      month: 'short',
+      day: 'numeric'
+    }
   };
 
-  if(dateString1 === dateString2) {
-    return new Date(dateString1).toLocaleString('it-IT', locale);
+  options = {...default_options, ...(options??{})};
+
+
+  if(dateIsoString1 === dateIsoString2) {
+    return new Date(dateIsoString1).toLocaleString(options.locale, options.locale_options);
 
   } else {
-    const [y1, m1, d1] = dateString1.substring(0, 10).split('-')
-      ,[y2, m2, d2] = dateString2.substring(0, 10).split('-')
+    const [y1, m1, d1] = dateIsoString1.substring(0, 10).split('-')
+      ,[y2, m2, d2] = dateIsoString2.substring(0, 10).split('-')
     ;
 
     if(y1 !== y2) {
-      return new Date(dateString1).toLocaleString('it-IT', locale) +
+      return new Date(dateIsoString1).toLocaleString(options.locale, options.locale_options) +
         ' &ndash; ' +
-        new Date(dateString2).toLocaleString('it-IT', locale);
+        new Date(dateIsoString2).toLocaleString(options.locale, options.locale_options);
 
     } else if (m1 !== m2) {
 
+      options.locale_options.year = undefined;
 
-      locale = {
-        month: 'short',
-        day: 'numeric'
-      };
-      return new Date(dateString1).toLocaleString('it-IT', locale) +
+      return new Date(dateIsoString1).toLocaleString(options.locale, options.locale_options) +
         ' &ndash; ' +
-        new Date(dateString2).toLocaleString('it-IT', locale) +
+        new Date(dateIsoString2).toLocaleString(options.locale, options.locale_options) +
         ` ${y1}`;
 
     } else {
-      locale = {
-        year: 'numeric',
-        month: 'short'
-      };
-      return `${d1}&ndash;${d2} ` + new Date(dateString1).toLocaleString('it-IT', locale);
+
+      options.locale_options.day = undefined;
+
+      return `${d1}&ndash;${d2} ` + new Date(dateIsoString1).toLocaleString(options.locale, options.locale_options);
     }
 
   }
