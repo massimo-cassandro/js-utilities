665568d38e91d49bfd8a8ffb5b381fd8b2af3e42
Massimo Cassandro <massimo.cassandro@gmail.com>
2025-07-18 15:45:39 +0200

    v.1.16.0 - array to csv

--- index.js
+++ index.js
@@ -31,3 +31,4 @@ export { formMultiselect } from './src/form-multiselect.js';
 export { getCookie, setCookie } from './src/cookies.js';
 export { default as relativeNextDate} from './src/date-relative.js';
 export { datesFromTo } from './src/dates-from-to.js';
+export { arrayToCsv } from './src/array-to-csv.js';
