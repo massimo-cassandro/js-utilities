270d1d73469bcef0ae3b89274c344ffb1a62d03c
Massimo Cassandro <massimo.cassandro@gmail.com>
2025-07-18 15:51:12 +0200

    v.1.16.1 - fix

--- src/array-to-csv.js
+++ src/array-to-csv.js
@@ -2,7 +2,7 @@
 
 export function arrayToCsv(csv_array) {
   return csv_array.map(row =>
-    row.map(cell => `"${cell.replace(/;/g, '').replace(/"/g, '""')}`).join(';')
+    row.map(cell => `"${cell/* .replace(/;/g, '') */.replace(/"/g, '""')}"`).join(';')
   ).join('\n');
 }
 
